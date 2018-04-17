c54177883966a6bd277b51897d4b7d752964a0b6
TheDechev <dechevmartin@yahoo.com>
2018-04-17 17:26:04 +0300

    Simple buttons added

---
+++ Number Wizard/Assets/Scripts/LevelManger.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManger : MonoBehaviour {
+
+    public void LevelLoad(string levelName)
+    {
+        Application.LoadLevel(levelName);
+    }
+
+    public void QuitRequest()
+    {
+        Application.Quit();
+    }
+
+    public void ReturnRequest()
+    {
+        Application.LoadLevel("Start");
+    }
+}
