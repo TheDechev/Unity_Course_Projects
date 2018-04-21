26a4634a996c5537dea62cae16639b85429fcad1
TheDechev <dechevmartin@yahoo.com>
2018-04-21 12:05:38 +0300

    Simple Music import

---
+++ Block Breaker/Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicPlayer : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        //actually means calling the DontDestroyOnLoad and using it on the gameObject (music Player in this case)
+        GameObject.DontDestroyOnLoad(gameObject);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
