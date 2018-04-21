9f2efd11a97cd5a0a762af8c1f23eb803eb78fff
TheDechev <dechevmartin@yahoo.com>
2018-04-22 01:43:46 +0300

    Introduction to colliders

--- Block Breaker/Assets/Scripts/MusicPlayer.cs
+++ Block Breaker/Assets/Scripts/MusicPlayer.cs
@@ -3,14 +3,30 @@ using System.Collections;
 
 public class MusicPlayer : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-        //actually means calling the DontDestroyOnLoad and using it on the gameObject (music Player in this case)
-        GameObject.DontDestroyOnLoad(gameObject);
+    static MusicPlayer instance = null;
+
+    void Awake()
+    {
+        if (instance != null)
+        {
+            Destroy(gameObject);
+            print("Duplicate music player self-destructing!");
+        }
+        else
+        {
+            instance = this;
+            GameObject.DontDestroyOnLoad(gameObject); //actually means calling the DontDestroyOnLoad and using it on the gameObject (music Player in this case)
+        }
+    }
+
+	void Start ()
+    {
+
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void Update ()
+    {
+
 	}
 }
