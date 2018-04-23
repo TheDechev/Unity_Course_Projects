b9a98d841a8c851e22227299c5476209cccb4cd2
TheDechev <dechevmartin@yahoo.com>
2018-04-23 17:40:26 +0300

    Physics added, the ball bounces on the paddle

---
+++ Block Breaker/Assets/Scripts/Ball.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ball : MonoBehaviour {
+
+    public Paddle paddleObj;
+    private bool hasStarted = false; 
+    private Vector3 paddleToBallVector;
+    private Rigidbody2D ballrigidbody2d;
+
+    // Use this for initialization
+    void Start () {
+        rigidbody2d = GetComponent<Rigidbody2D>();
+        paddleToBallVector = this.transform.position - paddleObj.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (!hasStarted)
+        {
+            this.transform.position = paddleToBallVector + paddleObj.transform.position;
+        }
+
+        if(Input.GetMouseButtonDown(0))
+        {
+            rigidbody2d.velocity = new Vector2(2f, 15f);
+            hasStarted = true;
+            print("mouse clicked");
+        } 
+	}
+}
---
+++ Block Breaker/Assets/Scripts/LoseCollider.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoseCollider : MonoBehaviour {
+
+    public LevelManger levelManagerObj;
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        print("Collision");
+    }
+
+    void OnTriggerEnter2D(Collider2D trigger)
+    {
+        print("Trigger");
+        levelManagerObj.LevelLoad("Win Screen");
+    }
+}
